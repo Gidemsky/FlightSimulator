910031b98ebd1c56e26117d0ac3ae436388bdcb8
Gideon Dembovsky <gidemsky26@gmail.com>
2019-04-22 23:14:17 +0300

    view viewmodel and model class added

--- FlightSimulator/FlightSimulator/MainWindow.xaml.cs
+++ FlightSimulator/FlightSimulator/MainWindow.xaml.cs
@@ -30,63 +30,66 @@ namespace FlightSimulator
         {
             InitializeComponent();
             my_comm = new DataCommunication();
-            db = new DataBinding(my_comm);
-            DataContext = db;
-            MyJoystick.Moved += MyJoystick_Moved;
-            MyJoystick.Released += MyJoystick_Released;
+            //        my_comm = new DataCommunication();
+            //        db = new DataBinding(my_comm);
+            //        DataContext = db;
+            //        MyJoystick.Moved += MyJoystick_Moved;
+            //        MyJoystick.Released += MyJoystick_Released;
 
-            
-            Thread thread = new Thread(new ParameterizedThreadStart(this.MyFunc));
-            Thread thread2 = new Thread(new ParameterizedThreadStart(this.MyFunc));
-            
-            thread.Start(5);
-            thread2.Start(199);
-        }
-        private void MyFunc(object nob)
-        {
-            int n = (int) nob;
-            for (int i = 0; i < 100; ++i)
-            {
-                n += 1;
-                Thread.Sleep(350 + new Random().Next(0, 350));
 
-                text_box.Dispatcher.Invoke(
-                    DispatcherPriority.Background,
-                    new Action(() => text_box.Text = n.ToString()));
-                
-            }
-        }
+            //        Thread thread = new Thread(new ParameterizedThreadStart(this.MyFunc));
+            //        Thread thread2 = new Thread(new ParameterizedThreadStart(this.MyFunc));
 
-        private void MyJoystick_Released(Joystick sender)
-        {
-            db.Elevator = 0.0;
-            db.Aileron = 0.0;
-            my_comm.Send_string("\\controller\\aileron", db.Aileron);
-        }
+            //        thread.Start(5);
+            //        thread2.Start(199);
+            //    }
+            //    private void MyFunc(object nob)
+            //    {
+            //        int n = (int) nob;
+            //        for (int i = 0; i < 100; ++i)
+            //        {
+            //            n += 1;
+            //            Thread.Sleep(350 + new Random().Next(0, 350));
 
-        private void MyJoystick_Moved(Joystick sender, Model.EventArgs.VirtualJoystickEventArgs args)
-        {
-            db.Elevator = args.Elevator;
-            db.Aileron = args.Aileron;
-            my_comm.Send_string("\\controller\\aileron", db.Aileron);
-        }
+            //            text_box.Dispatcher.Invoke(
+            //                DispatcherPriority.Background,
+            //                new Action(() => text_box.Text = n.ToString()));
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            text_box.Background = Brushes.PaleVioletRed;
+            //        }
+            //    }
 
-        }
+            //    private void MyJoystick_Released(Joystick sender)
+            //    {
+            //        db.Elevator = 0.0;
+            //        db.Aileron = 0.0;
+            //        my_comm.Send_string("\\controller\\aileron", db.Aileron);
+            //    }
 
-        private void Button_Clear(object sender, RoutedEventArgs e)
-        {
+            //    private void MyJoystick_Moved(Joystick sender, Model.EventArgs.VirtualJoystickEventArgs args)
+            //    {
+            //        db.Elevator = args.Elevator;
+            //        db.Aileron = args.Aileron;
+            //        my_comm.Send_string("\\controller\\aileron", db.Aileron);
+            //    }
 
-            text_box.Background = Brushes.White;
+            //    private void Button_Click(object sender, RoutedEventArgs e)
+            //    {
+            //        text_box.Background = Brushes.PaleVioletRed;
 
-        }
+            //    }
 
-        private void MyJoystick_Loaded(object sender, RoutedEventArgs e)
-        {
+            //    private void Button_Clear(object sender, RoutedEventArgs e)
+            //    {
 
+            //        text_box.Background = Brushes.White;
+
+            //    }
+
+            //    private void MyJoystick_Loaded(object sender, RoutedEventArgs e)
+            //    {
+
+            //    }
+            //}
         }
     }
 }
---
+++ FlightSimulator/FlightSimulator/Model/ServerConnect.cs
@@ -0,0 +1,96 @@
+﻿using FlightSimulator.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FlightSimulator.Model
+{
+    class ServerConnect : BaseNotify
+    {
+        private static ServerConnect self;
+        private TcpListener tcpListener = null;
+        bool isConnceted;
+        private string socketData;
+
+        public static ServerConnect Instance
+        {
+            get
+            {
+                if (self == null)
+                {
+                    self = new ServerConnect();
+                }
+                return self;
+            }
+        }
+
+        public string Data
+        {
+            get
+            {
+                return socketData;
+            }
+            set
+            {
+                socketData = value;
+                NotifyPropertyChanged("Data");
+            }
+        }
+
+        public void createConnection(string ip, int port)
+        {
+            try
+            {
+                IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ip), port);
+                tcpListener = new TcpListener(ep);
+                tcpListener.Start();
+                Console.WriteLine("Waiting for incoming connections...");//TODO: erase or change the msg
+                TcpClient client = tcpListener.AcceptTcpClient();
+                Console.WriteLine("Accepted Client !");//TODO: erase or change the msg
+                NetworkStream stream = client.GetStream();
+                Thread t = new Thread(() => {
+                    while (!isConnceted)
+                    {
+                        if (stream.DataAvailable)
+                        {
+                            byte[] line = new byte[512];//TODO: create constant
+                            int read = stream.Read(line, 0, line.Length);
+                            string parsedData = Encoding.UTF8.GetString(line, 0, read);
+                            Data = parsedData;
+                        }
+                    }
+                    stream.Close();
+                    client.Close();
+                });
+                t.Start();
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("ERROR");
+            }
+            finally
+            {
+                tcpListener.Stop();
+            }
+
+        }
+
+        public void Close()
+        {
+            isConnceted = true;
+        }
+
+        public bool IsOpen()
+        {
+            return this.tcpListener != null;
+        }
+
+
+    }
+}
