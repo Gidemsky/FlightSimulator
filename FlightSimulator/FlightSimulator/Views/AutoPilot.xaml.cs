b2e279074a6b0658846d07e663fb3edccdf9f86a
davidbend <bnddavid750@gmail.com>
2019-04-22 23:18:56 +0300

    View updated

--- FlightSimulator/FlightSimulator/Views/AutoPilot.xaml.cs
+++ FlightSimulator/FlightSimulator/Views/AutoPilot.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,18 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Threading;
-using FlightSimulator.Views;
 
-namespace FlightSimulator
+namespace FlightSimulator.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AutoPilot.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AutoPilot : UserControl
     {
-        public MainWindow()
+        public AutoPilot()
         {
             InitializeComponent();
         }
