686ab02e93063a24d42649e7a65a32c917da4ea0
Gideon Dembovsky <gidemsky26@gmail.com>
2019-04-22 16:59:24 +0300

    class com added

---
+++ FlightSimulator/FlightSimulator/FlightBoardBinding.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulator
+{
+    class FlightBoardBinding : INotifyPropertyChanged
+    {
+        double latitude;
+        double longitude;
+
+        public double Latitude
+        {
+            get
+            {
+                return latitude;
+            }
+            set
+            {
+                latitude = value;
+                OnPropertyChanged("LatitudeString");
+            }
+        }
+
+        public double Longitude
+        {
+            get
+            {
+                return longitude;
+            }
+            set
+            {
+                longitude = value;
+                OnPropertyChanged("LongitudeString");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
---
+++ FlightSimulator/FlightSimulator/Model/InfoCommunication.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulator
+{
+    class InfoCommunication
+    {
+        readonly Socket sock;
+        public InfoCommunication()
+        {
+            string ip = FlightSimulator.Properties.Settings.Default.FlightServerIP;
+            int port = FlightSimulator.Properties.Settings.Default.FlightCommandPort;
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sock.Connect(new IPEndPoint(IPAddress.Parse(ip), port));
+        }
+
+        public void get_string(string path)
+        {
+            byte[] incomeMsg = new byte[200];
+            int k = sock.Receive(incomeMsg);
+            string msg = Encoding.ASCII.GetString(incomeMsg, 0, k);
+            //string formatter = "set {0}={1}\r\n";
+            //string message = String.Format(formatter, path, value);
+            //byte[] bytes = Encoding.ASCII.GetBytes(message);
+            //sock.Send(bytes);
+        }
+
+        public void close()
+        {
+            sock.Close();
+        }
+    }
+}
